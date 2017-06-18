b0cae65ba6842ee77912a2d7d5a36b6cda168f43
Navdeep Singh <Navdeep Singh>
2017-06-18 09:07:15 +0530

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("How many hours did you sleep last night?");
+            int HoursOfSleep = int.Parse( Console.ReadLine());
+            Console.WriteLine("Hello "+name);
+
+            if (HoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more rest");
+            }
+            
+        }
+    }
+}
